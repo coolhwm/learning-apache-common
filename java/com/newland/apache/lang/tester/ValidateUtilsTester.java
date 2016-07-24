58c798e188b847e05c5d247693590fc0c561195f
Huang WeiMing <coolhwm@qq.com>
2016-07-24 13:18:32 +0800

    idea commit

--- java/com/newland/apache/lang/tester/ValidateUtilsTester.java
+++ java/com/newland/apache/lang/tester/ValidateUtilsTester.java
@@ -1,7 +1,23 @@
 package com.newland.apache.lang.tester;
 
+import org.apache.commons.lang3.Validate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
 /**
- * Created by 伟明 on 2016/7/23.
+ *
  */
+@RunWith(BlockJUnit4ClassRunner.class)
 public class ValidateUtilsTester {
+
+    /**
+     *
+     */
+    @Test
+    public void isEmpty(){
+        Object obj = null;
+        Validate.notNull(obj, "The input value is null");
+    }
+
 }
